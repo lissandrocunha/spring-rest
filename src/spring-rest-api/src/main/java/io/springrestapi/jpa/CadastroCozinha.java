b3b62f89994fa586ba00c06af12cdde3b2fdbb8a
Lissandro Perossi Cunha <lissandro_cunha@yahoo.com.br>
2021-08-04 09:33:08 -0300

    concluído aula 3.9

---
+++ src/spring-rest-api/src/main/java/io/springrestapi/jpa/CadastroCozinha.java
@@ -0,0 +1,28 @@
+package io.springrestapi.jpa;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import io.springrestapi.domain.model.Cozinha;
+
+@Component
+public class CadastroCozinha {
+
+	@PersistenceContext
+	private EntityManager manager;
+	
+	public List<Cozinha> listar() {
+		return manager.createQuery("from Cozinha", Cozinha.class)
+				.getResultList();
+	}
+
+	@Transactional
+	public Cozinha adicionar(Cozinha cozinha) {
+		return manager.merge(cozinha);		
+	}
+}
