f3c1e857a0c998942d288a1b235eb8d30b8a5161
Lissandro Perossi Cunha <lissandro_cunha@yahoo.com.br>
2021-08-10 14:46:44 -0300

    concluído aula 4.22

--- src/spring-rest-api/src/main/java/io/springrestapi/api/controller/CozinhaController.java
+++ src/spring-rest-api/src/main/java/io/springrestapi/api/controller/CozinhaController.java
@@ -3,13 +3,18 @@ package io.springrestapi.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.springrestapi.api.model.CozinhasXmlWrapper;
 import io.springrestapi.domain.model.Cozinha;
 import io.springrestapi.domain.repository.CozinhaRepository;
 
@@ -26,9 +31,27 @@ public class CozinhaController {
 		return cozinhaRepository.listar();
 	}
 
+	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+	public CozinhasXmlWrapper listarXml() {
+		return new CozinhasXmlWrapper(cozinhaRepository.listar());
+	}
+
 	@GetMapping(value = "/{cozinhaId}")
-	public Cozinha Buscar(@PathVariable Long cozinhaId) {
-		return cozinhaRepository.buscar(cozinhaId);
+	public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
+		Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+
+		if (cozinha != null) {
+			return ResponseEntity.ok(cozinha);
+		}
+
+		return ResponseEntity.notFound().build();
+
+	}
+
+	@PostMapping
+	public void adicionar(@RequestBody Cozinha cozinha) {
+		cozinhaRepository.salvar(cozinha);
+
 	}
 
 }
