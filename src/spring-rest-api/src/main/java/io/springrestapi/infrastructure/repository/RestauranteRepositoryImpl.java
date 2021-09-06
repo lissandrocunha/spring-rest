93e75738a9f831a5c6629778eb0ebe65bb007bb6
Lissandro Perossi Cunha <lissandro_cunha@yahoo.com.br>
2021-09-06 14:53:03 -0300

    concluido aula 5.15

--- src/spring-rest-api/src/main/java/io/springrestapi/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/spring-rest-api/src/main/java/io/springrestapi/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -1,6 +1,7 @@
 package io.springrestapi.infrastructure.repository;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -12,6 +13,7 @@ import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import io.springrestapi.domain.model.Restaurante;
 import io.springrestapi.domain.repository.RestaurantesRepositoryQueries;
@@ -30,11 +32,19 @@ public class RestauranteRepositoryImpl implements RestaurantesRepositoryQueries 
 		CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
 		Root<Restaurante> root = criteria.from(Restaurante.class);
 
-		Predicate nomePredicate = builder.like(root.get("nome"), "%" + nome + "%");
-		Predicate taxaIninical = builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial);
-		Predicate taxaFinal = builder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal);
+		var predicates = new ArrayList<Predicate>();
 
-		criteria.where(nomePredicate,taxaIninical, taxaFinal);
+		if (StringUtils.hasLength(nome)) {
+			predicates.add(builder.like(root.get("nome"), "%" + nome + "%"));
+		}
+		if (taxaFreteInicial != null) {
+			predicates.add(builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial));
+		}
+		if (taxaFreteFinal != null) {
+			predicates.add(builder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal));
+		}
+
+		criteria.where(predicates.toArray(new Predicate[0]));
 
 		TypedQuery<Restaurante> query = manager.createQuery(criteria);
 
