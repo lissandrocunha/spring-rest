ab02a3e44b31f1ccf0303fa0b6f873e7a0b7a201
Lissandro Perossi Cunha <lissandro_cunha@yahoo.com.br>
2021-09-06 16:28:12 -0300

    concluido aula 6.1

--- src/spring-rest-api/src/main/java/io/springrestapi/domain/model/Cozinha.java
+++ src/spring-rest-api/src/main/java/io/springrestapi/domain/model/Cozinha.java
@@ -1,10 +1,16 @@
 package io.springrestapi.domain.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -22,4 +28,7 @@ public class Cozinha {
 	@Column(name = "nome", nullable = false)
 	private String nome;
 
+	@JsonIgnore
+	@OneToMany(mappedBy = "cozinha")
+	private List<Restaurante> restaurantes = new ArrayList<>();
 }
